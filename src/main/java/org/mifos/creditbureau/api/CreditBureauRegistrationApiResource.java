2ac9327c1200da112bde609001552e14c97b510f
yuwatidora <yuwatinyi003@gmail.com>
2025-07-14 17:37:01 -0400

    controller updates

--- src/main/java/org/mifos/creditbureau/api/CreditBureauRegistrationApiResource.java
+++ src/main/java/org/mifos/creditbureau/api/CreditBureauRegistrationApiResource.java
@@ -6,30 +6,41 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import lombok.AllArgsConstructor;
 import org.mifos.creditbureau.data.CBRegisterParamsData;
+import org.mifos.creditbureau.data.CreditBureauData;
 import org.mifos.creditbureau.domain.CBRegisterParams;
+import org.mifos.creditbureau.service.CreditBureauRegistrationReadService;
 import org.mifos.creditbureau.service.CreditBureauRegistrationWriteImplServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Path("/CreditBureauRegistration")
 public class CreditBureauRegistrationApiResource {
     private final CreditBureauRegistrationWriteImplServiceImpl creditBureauRegistrationWriteService;
+    private final CreditBureauRegistrationReadService creditBureauRegistrationReadService;
 
     @GET
     @Path("/available")
-    public List<String> getAvailableCreditBureaus() {
-        return null;
+    public List<String> getAllCreditBureaus() {
+        // Fetch the list of CreditBureauData objects
+        List<CreditBureauData> creditBureaus = this.creditBureauRegistrationReadService.getAllCreditBureaus();
+
+        // Transform the list into a list of names using a Java Stream
+        return creditBureaus.stream()
+                .map(CreditBureauData::getCreditBureauName)
+                .collect(Collectors.toList());
     }
 
     @GET
     @Path("/{id}/configParams")
-    public List<String> getConfigParams(@PathParam("id") Long organizationCreditBureauId) {
-        return null;
-
+    //TODO: Complete this for later
+    public List<String> getConfigParams(Long organizationCreditBureauId) {        // Fetch the configuration parameters for the given ID
+        return new ArrayList<>();
     }
 
     @POST
