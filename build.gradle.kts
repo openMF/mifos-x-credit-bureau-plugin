90df289264a394cf25c55f84eba09157e0217828
yuwatidora <yuwatinyi003@gmail.com>
2025-07-25 16:14:55 -0400

    variable name change

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ java {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
+}
 
 configurations {
 	compileOnly {
@@ -31,31 +35,46 @@ ext {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	// Spring Boot Starters
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
-	implementation("org.glassfish.jersey.core:jersey-server:3.1.5")
-	implementation("org.glassfish.jersey.containers:jersey-container-servlet:3.1.5")
-	implementation("org.glassfish.jersey.inject:jersey-hk2:3.1.5")
-	implementation("org.liquibase:liquibase-core")
-	implementation("org.mapstruct:mapstruct:1.6.3")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// API Documentation
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
-	implementation("org.apache.fineract:fineract-core:${project.ext["fineractVersion"]}")
-//	implementation("org.apache.fineract:fineract-provider:${project.ext["fineractVersion"]}:plain")
+	// Jakarta REST / Jersey (JAX-RS)
+	implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
+	implementation("org.glassfish.jersey.containers:jersey-container-servlet:3.1.5")
+	implementation("org.glassfish.jersey.core:jersey-server:3.1.5")
+	implementation("org.glassfish.jersey.inject:jersey-hk2:3.1.5")
 
+	// Database & ORM
+	implementation("org.liquibase:liquibase-core")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	compileOnly("org.projectlombok:lombok")
 
-	annotationProcessor("org.projectlombok:lombok")
+	// Fineract
+	implementation("org.apache.fineract:fineract-core:${project.ext["fineractVersion"]}")
+	// Optional alternative:
+	// implementation("org.apache.fineract:fineract-provider:${project.ext["fineractVersion"]}:plain")
+
+	// Mapping / Code Generation
+	implementation("org.mapstruct:mapstruct:1.6.3")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
+	// Cryptography
+	implementation("org.bouncycastle:bcprov-jdk18on:1.78")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	// Testing
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testRuntimeOnly("com.h2database:h2")
-	testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
--- src/main/java/org/mifos/creditbureau/data/CreditBureauData.java
+++ src/main/java/org/mifos/creditbureau/data/CreditBureauData.java
@@ -1,10 +1,14 @@
 package org.mifos.creditbureau.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder(toBuilder = true)
+@AllArgsConstructor
 public class CreditBureauData {
 
     private final long id;
@@ -17,6 +21,6 @@ public class CreditBureauData {
 
     private final String country;
 
-    private final CBRegisterParamsData cbRegisterParamsData;
+    private final CBRegisterParamsData creditBureauParameter;
 
 }
