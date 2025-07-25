f4147476ed2037331e3a193e4aa25bcbc1338a40
yuwatidora <yuwatinyi003@gmail.com>
2025-07-25 16:32:32 -0400

    dto updates

--- src/main/java/org/mifos/creditbureau/data/ClientAddressData.java
+++ src/main/java/org/mifos/creditbureau/data/ClientAddressData.java
@@ -1,4 +1,11 @@
 package org.mifos.creditbureau.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class ClientAddressData {
+
+    private final String streetAddress;
 }
--- src/main/java/org/mifos/creditbureau/data/ClientData.java
+++ src/main/java/org/mifos/creditbureau/data/ClientData.java
@@ -1,4 +1,24 @@
 package org.mifos.creditbureau.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 //Reformat data from fineract-client api for internal
+@Getter
+@AllArgsConstructor
 public class ClientData {
+
+    private final long id;
+
+    private final String clientFirstName;
+
+    private final String clientLastName;
+
+    private final String clientBirthDate;
+
+    private final String RFC;
+
+    private final String nationality;
+
+    private final String address;
 }
