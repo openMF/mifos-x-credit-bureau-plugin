4fc92fd2d057151356030782e64a5925ffa1ff28
yuwatidora <yuwatinyi003@gmail.com>
2025-08-12 14:21:44 -0400

    remove kotlin wrapper

--- src/main/java/org/mifos/creditbureau/service/ClientApiService.java
+++ src/main/java/org/mifos/creditbureau/service/ClientApiService.java
@@ -3,17 +3,11 @@ package org.mifos.creditbureau.service;
 import lombok.AllArgsConstructor;
 import org.mifos.creditbureau.data.ClientData;
 import org.springframework.stereotype.Service;
-import org.mifos.creditbureau.wrappers.FineractClientServiceWrapper;
-import org.mifos.creditbureau.data.ClientData;
-import org.mifos.creditbureau.data.ClientAddressData;
 
-//TODO: use a builder to populate the dtos
 //call this service, and use the populated dtos to construct a dto for a credit bureau
 @AllArgsConstructor
 @Service
 public class ClientApiService {
-
-    private final FineractClientServiceWrapper fineractClientServiceWrapper;
 
 
 }
