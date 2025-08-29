bed6bad7dffcf9102a080d874232ecbe89f97ed3
yuwatidora <yuwatinyi003@gmail.com>
2025-08-29 11:09:36 -0400

    add docs

--- src/main/java/org/mifos/creditbureau/config/JerseyConfig.java
+++ src/main/java/org/mifos/creditbureau/config/JerseyConfig.java
@@ -1,6 +1,7 @@
 package org.mifos.creditbureau.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import org.springframework.context.annotation.Configuration;
 
 import jakarta.ws.rs.ApplicationPath;
@@ -11,5 +12,6 @@ public class JerseyConfig extends ResourceConfig {
     
     public JerseyConfig() {
         packages("org.mifos.creditbureau.api");
+        register(OpenApiResource.class);
     }
 }
